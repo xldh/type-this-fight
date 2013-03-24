d7b41e7279cdabc390a9bc094cb91f73c05e7ac3
Axel Duch <axlduch@gmail.com>
2013-03-24 04:30:19 +0100

    Attack and Defend methods skeleton for RobotClass

--- public/js/RobotClass.js
+++ public/js/RobotClass.js
@@ -22,13 +22,35 @@ var RobotClass = Class.extend({
         this.upperDef = upperDef;
         this.delay = delay;
     },
+    attack: function (rivalId, zone) {
+    	console.log('rivalId: "' + rivalId + '" zone: "' + zone + '"');
+    	switch (zone) {
+		case 'lower':
+			break;
+		case 'upper':
+			break;
+		default:
+			break;
+    	}
+    },
+    defend: function (zone) {
+    	console.log('zone: "' + zone + '"');
+    	switch (zone) {
+		case 'lower':
+			break;
+		case 'upper':
+			break;
+		default:
+			break;
+    	}
+    },
     /**
      * Executes a given command
      * @throws Error if command is not available
      */
     exec: function (cmd) {
-        console.log('exec("' + cmd + "')");
-        ((RobotClass.availableCommands())[cmd] 
+        console.log('exec("' + cmd + '")');
+        ((RobotClass.availableCommands())[cmd]
         || (function () { throw new Error('Invalid command'); }()))
             .apply(this, [this.id, this.rivalId]);
     },
@@ -38,18 +60,42 @@ var RobotClass = Class.extend({
 });
 
 RobotClass.availableCommands = function () {
+	var robot;
     return {
-        'att upper': function (attackerId, defenderId) {
-            (gGame.robots[attackerId]).attack(defenderId, 'upper');
+        'att upper': function (selfId, rivalId) {
+            robot = RobotClass.find(selfId);
+            if (robot) {
+            	robot.attack(rivalId, 'upper');
+            }
         },
-        'att lower': function (attackerId, defenderId) {
-            (gGame.robots[attackerId]).attack(defenderId, 'lower');
+        'att lower': function (selfId, rivalId) {
+            robot = RobotClass.find(selfId);
+            if (robot) {
+            	robot.attack(rivalId, 'lower');
+            }
         },
-        'def upper': function (attackerId, defenderId) {
-            (gGame.robots[defenderId]).increaseDef('upper');
+        'def upper': function (selfId) {
+            robot = RobotClass.find(selfId);
+            if (robot) {
+            	robot.defend('upper');
+            }
         },
-        'def lower': function (attackerId, defenderId) {
-            (gGame.robots[defenderId]).increaseDef('lower');
+        'def lower': function (selfId) {
+            robot = RobotClass.find(selfId);
+            if (robot) {
+            	robot.defend('lower');
+            }
         }
     };
 }
+RobotClass.find = function (id) {
+	var index;
+	gGame.robots.map(function(robot, key) {
+		if (robot.id === id) {
+			index = key;
+		}
+	});
+	if (index !== undefined) {
+		return gGame.robots[index];
+	}
+};
